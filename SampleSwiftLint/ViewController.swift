019487b7a78b875efba1bcf7a9a46f4b2cd5f35d
Aji Saputra Raka Siwi <ajisaputrars@hotmail.com>
2020-10-25 17:49:20 +0700

    Initial Commit

---
+++ SampleSwiftLint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleSwiftLint
+//
+//  Created by Aji Saputra Raka Siwi on 25/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
